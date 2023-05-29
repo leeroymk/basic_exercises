18bd14e21e00e5e3314373a668111a3fe593ed67
leeroymk <nikekursk@yandex.ru>
2023-05-29 23:20:29 +0300

    4/5 done

--- for_dict_challenges_bonus.py
+++ for_dict_challenges_bonus.py
@@ -37,7 +37,7 @@ import datetime
 import lorem
 
 
-def generate_chat_history():
+def generate_chat_history() -> list:
     messages_amount = random.randint(200, 1000)
     users_ids = list(
         {random.randint(1, 10000) for _ in range(random.randint(5, 20))}
@@ -66,5 +66,57 @@ def generate_chat_history():
     return messages
 
 
+def content_maker(messages: list[dict]) -> str:
+    writer: list = [messages[i]['sent_by'] for i in range(len(messages))]
+    max_writer: int = max(writer, key=writer.count)
+    ids_max_writers: set = set([i for i in writer if writer.count(i) == writer.count(max_writer)])
+    res: str = ', '.join(map(str, (set(ids_max_writers))))
+    return f'User ID, который написал больше всех сообщений - {res}.'
+
+
+def reply_maker(messages: list[dict]) -> str:
+    reply_ids: list = [messages[i]['reply_for'] for i in range(len(messages)) if messages[i]['reply_for']]
+    ids_replied: list = [messages[i]['sent_by'] for i in range(len(reply_ids)) if messages[i]['sent_by']]
+    try:
+        max_replied: int = max(ids_replied, key=ids_replied.count)
+    except ValueError as ve:
+        max_replied: None = None
+    return f'User ID, на сообщения которого больше всего отвечали - {max_replied}.'
+
+
+def influencer(messages: list[dict]) -> str:
+    users_seen_max: list = max([len(messages[i]['seen_by']) for i in range(len(messages)) if messages[i]['seen_by']])
+    res: list= [messages[i]['sent_by'] for i in range(len(messages)) if len(messages[i]['seen_by']) == users_seen_max]
+    ids_seen_max: set = ', '.join(map(str, (set(res))))
+    return f'User ID, сообщения которых видело больше всего уникальных пользователей - {ids_seen_max}.'
+
+
+def when_they_chat(messages: list[dict]) -> str:
+    msg_count: dict = {
+        'утром': 0,
+        'днем': 0,
+        'вечером': 0,
+        }
+    for i in range(len(messages)):
+        if messages[i]['sent_at'].hour in range(12):
+            msg_count['утром'] += 1
+        elif messages[i]['sent_at'].hour in range(12, 18):
+            msg_count['днем'] += 1
+        else:
+            msg_count['вечером'] += 1
+
+    max_msg_by_hour: str = ', '.join([k for k, v in msg_count.items() if v == max(msg_count.values())])
+    return f'В чате больше всего сообщений: {max_msg_by_hour}.'
+
+
+def threads(messages: list[dict]) -> str:
+    for i in range(len(messages)):
+        print(messages[i]['reply_for'])
+
+
 if __name__ == "__main__":
     print(generate_chat_history())
+    print(content_maker(messages=generate_chat_history()))
+    print(reply_maker(messages=generate_chat_history()))
+    print(influencer(messages=generate_chat_history()))
+    print(when_they_chat(messages=generate_chat_history()))
